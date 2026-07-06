615b74d8d95f32a8761c432d9453d76b7566cf60
Alexandr Pliushchou <alexandr.pliushchou@apryse.com>
2026-07-06 09:57:25 +0300

    Add css attribute as literal value test
    DEVSIX-10019

---
+++ src/test/java/com/itextpdf/html2pdf/css/selector/item/AttributeValueAsLiteralTest.java
@@ -0,0 +1,81 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2026 Apryse Group NV
+    Authors: Apryse Software.
+
+    This program is offered under a commercial and under the AGPL license.
+    For commercial licensing, contact us at https://itextpdf.com/sales.  For AGPL licensing, see below.
+
+    AGPL licensing:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.itextpdf.html2pdf.css.selector.item;
+
+import com.itextpdf.html2pdf.ExtendedHtmlConversionITextTest;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.layout.element.IElement;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.properties.Property;
+import com.itextpdf.layout.properties.TransparentColor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+@Tag("UnitTest")
+public class AttributeValueAsLiteralTest extends ExtendedHtmlConversionITextTest {
+
+    @Test
+    public void valueIsLiteralNotEqualTest() {
+
+        String html = "<html>" +
+                "<head>" +
+                "<style>" +
+                "[data-info~=\"a.c\"] { color:red; }" +
+                "</style>" +
+                "</head>" +
+                "<body>" +
+                "<p data-info=\" abc\">Text</p>" +
+                "</body>" +
+                "</html>";
+
+        List<IElement> elements = HtmlConverter.convertToElements(html);
+        Paragraph p = (Paragraph) elements.get(0);
+        TransparentColor color = p.<TransparentColor>getProperty(Property.FONT_COLOR);
+
+        Assertions.assertNull(color);
+    }
+
+    @Test
+    public void valueIsLiteralEqualTest() {
+
+        String html = "<html>" +
+                "<head>" +
+                "<style>" +
+                "[data-info~=\"a]c\"] { color:red; }" +
+                "</style>" +
+                "</head>" +
+                "<body>" +
+                "<p data-info=\" a]c\">Text</p>" +
+                "</body>" +
+                "</html>";
+
+        List<IElement> elements = HtmlConverter.convertToElements(html);
+        Paragraph p = (Paragraph) elements.get(0);
+        TransparentColor color = p.<TransparentColor>getProperty(Property.FONT_COLOR);
+
+        Assertions.assertArrayEquals(new float[]{1f, 0f, 0f}, color.getColor().getColorValue());
+    }
+}
